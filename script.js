2f415e220ac3763eab2930c56e5c26c612c08fbe
Sharan kumar <bssharankumar28920@gmail.com>
2025-04-10 12:48:53 +0530

    function get computer choice over

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // console.log("hiii");
+function getComputerChoice() {
+  let random_number = Math.trunc(Math.random() * 3);
+  if (random_number === 0) {
+    return "rock";
+  } else if (random_number === 1) {
+    return "paper";
+  } else {
+    return "Scissors";
+  }
+}
+console.log(getComputerChoice());
